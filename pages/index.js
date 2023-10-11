437e20fe22cdc288f16a8dd6554cdb1b05d75e2a
gautamsharan <gautam.sharan@collabera.com>
2023-10-11 13:52:29 +0530

    example of getStaticProps and getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
+import Link from "next/link";
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import { getSortedProductsData } from "../lib/products";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  const allProductsData = getSortedProductsData();
+  return {
+    props: {
+      allPostsData,
+      allProductsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData, allProductsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[My Self Introduction]</p>
+        <p>I am a robot 🤖</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -18,6 +33,30 @@ export default function Home() {
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Shop</h2>
+        <ul className={utilStyles.list}>
+          {allProductsData.map(({ id, title, price }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/products/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>${price}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
